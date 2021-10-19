1f20553f6cb90f828df104963a3e5998ddbb74ac
gilgardosh <gilgardosh@gmail.com>
2021-10-19 14:55:07 +0300

    some fixes

--- src/utils/dataHandlers.ts
+++ src/utils/dataHandlers.ts
@@ -8,8 +8,8 @@ const digitsAdjuster = (value: string, length: number) => {
 export const transactionHandler = (transaction: Transaction): Transaction => {
   if (transaction.refNumber && transaction.refNumber.length !== 9) {
     console.debug(
-      `Expected Transaction refNumber to be of ${length} digits, received "${transaction.refNumber}". ${
-        transaction.refNumber.length > 9 ? `Using the last ${length} digits.` : `Adding leading zeros.`
+      `Expected Transaction refNumber to be of 9 digits, received "${transaction.refNumber}". ${
+        transaction.refNumber.length > 9 ? `Using the last 9 digits.` : `Adding leading zeros.`
       }`,
     );
     transaction.refNumber = digitsAdjuster(transaction.refNumber, 9);
@@ -17,8 +17,8 @@ export const transactionHandler = (transaction: Transaction): Transaction => {
 
   if (transaction.vatId && transaction.vatId.length !== 9) {
     console.debug(
-      `Expected Transaction vatId to be of ${length} digits, received "${transaction.vatId}". ${
-        transaction.vatId.length > 9 ? `Using the last ${length} digits.` : `Adding leading zeros.`
+      `Expected Transaction vatId to be of 9 digits, received "${transaction.vatId}". ${
+        transaction.vatId.length > 9 ? `Using the last 9 digits.` : `Adding leading zeros.`
       }`,
     );
     transaction.vatId = digitsAdjuster(transaction.vatId, 9);
@@ -26,8 +26,8 @@ export const transactionHandler = (transaction: Transaction): Transaction => {
 
   if (transaction.refGroup && transaction.refGroup.length !== 4) {
     console.debug(
-      `Expected Transaction refGroup to be of ${length} digits, received "${transaction.refGroup}". ${
-        transaction.refGroup.length > 9 ? `Using the last ${length} digits.` : `Adding leading zeros.`
+      `Expected Transaction refGroup to be of 4 digits, received "${transaction.refGroup}". ${
+        transaction.refGroup.length > 9 ? `Using the last 4 digits.` : `Adding leading zeros.`
       }`,
     );
     transaction.refGroup = digitsAdjuster(transaction.refGroup, 4);
@@ -39,8 +39,8 @@ export const transactionHandler = (transaction: Transaction): Transaction => {
 export const headerHandler = (header: Header): Header => {
   if (header.licensedDealerId && header.licensedDealerId.length !== 9) {
     console.debug(
-      `Expected Header licensedDealerId to be of ${length} digits, received "${header.licensedDealerId}". ${
-        header.licensedDealerId.length > 9 ? `Using the last ${length} digits.` : `Adding leading zeros.`
+      `Expected Header licensedDealerId to be of 9 digits, received "${header.licensedDealerId}". ${
+        header.licensedDealerId.length > 9 ? `Using the last 9 digits.` : `Adding leading zeros.`
       }`,
     );
     header.licensedDealerId = digitsAdjuster(header.licensedDealerId, 9);
